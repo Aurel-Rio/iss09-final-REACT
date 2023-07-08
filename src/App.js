3d6a002fd34ca27ac256f27c6a8587d90babd574
Aurel-Rio <servant.aurelien.developer@gmail.com>
2023-07-08 06:32:11 +0200

    iss09-v3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IntroAnimation from './components/IntroAnimation';
 import Accueil from './components/Accueil';
@@ -9,31 +9,38 @@ import Assistance from './components/Assistance';
 import Contact from './components/Contact';
 import './container.css';
 import back02 from './assets/back02.png';
+import { StyleSheetManager } from 'styled-components';
 
 function App() {
   const [showIntro, setShowIntro] = useState(true);
 
-  const handleAnimationComplete = () => {
+  const handleAnimationComplete = useCallback(() => {
     setShowIntro(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    // Code de l'effet
+  }, [handleAnimationComplete]);
 
   return (
-    <Router>
-      <div className={showIntro ? "intro-container" : ""}>
-        {showIntro ? null : <Navbar />}
-        <Routes>
-          {showIntro && (
-            <Route path="/" element={<IntroAnimation onAnimationComplete={handleAnimationComplete} />} />
-          )}
-          {!showIntro && <Route path="/" element={<Accueil />} />}
-          <Route path="/forfaits" element={<Forfaits />} />
-          <Route path="/assistance" element={<Assistance />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        {showIntro ? null : <Footer />}
-      </div>
-      {!showIntro && <img src={back02} alt="Background" className="background-image" />}
-    </Router>
+    <StyleSheetManager target={document.head}>
+      <Router>
+        <div className={showIntro ? "intro-container" : ""}>
+          {showIntro ? null : <Navbar />}
+          <Routes>
+            {showIntro && (
+              <Route path="/" element={<IntroAnimation onAnimationComplete={handleAnimationComplete} />} />
+            )}
+            {!showIntro && <Route path="/" element={<Accueil />} />}
+            <Route path="/forfaits" element={<Forfaits />} />
+            <Route path="/assistance" element={<Assistance />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          {showIntro ? null : <Footer />}
+        </div>
+        {!showIntro && <img src={back02} alt="Background" className="background-image" />}
+      </Router>
+    </StyleSheetManager>
   );
 }
 
